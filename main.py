17884a6e8c66c8fe9a846f58725ae31cf60fa2d7
Orion9551 <emmanuelombo95@gmail.com>
2023-03-26 20:51:32 +0100

    Coding the create view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask
-from models import db, PatientModel, AppointmentModel
-
+from flask import Flask, request, redirect, render_template
+from models import db, PatientModel  # AppointmentModel
 
 app = Flask(__name__)
 
@@ -15,5 +14,20 @@ db.init_app(app)
 def create_table():
     db.create_all()
 
-
 app.run(host='localhost', port = 7000)
+
+# Coding the Create view
+@app.route('/data/create' , methods = ['GET','POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('createpage.html')
+ 
+    if request.method == 'POST':
+        nhs_number = request.form['nhs_number']
+        name = request.form['name']
+        dob = request.form['dob']
+        postcode = request.form['postcode']
+        patient = PatientModel(nhs_number=nhs_number, name=name, dob=dob, postcode = postcode)
+        db.session.add(patient)
+        db.session.commit()
+        return redirect('/data')
--- models.py
+++ models.py
@@ -10,13 +10,13 @@ class PatientModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nhs_number = db.Column(db.Integer(), unique = True)
     name = db.Column(db.String())
-    DOB = db.Column(db.String(12))
+    dob = db.Column(db.String())
     postcode = db.Column(db.String(10))
 
-    def __init__(self, nhs_number,name,DOB,postcode):
+    def __init__(self, nhs_number,name,dob,postcode):
         self.nhs_number = nhs_number
         self.name = name
-        self.DOB = DOB
+        self.dob = dob
         self.postcode = postcode
 
     def __repr__(self):
@@ -24,24 +24,24 @@ class PatientModel(db.Model):
 
 
 # Create Appointment Model
-class AppointmentModel(db.Model):
-    __tablename__ = "table"
+# class AppointmentModel(db.Model):
+#     __tablename__ = "table"
 
-    id = db.Column(db.Integer, primary_key=True)
-    patient = db.Column()
-    status = db.Column(db.String())
-    time = db.Column(db.String(12))
-    duration = db.Column(db.String(10))
-    clinician = db.Column(db.String(50))
-    department = db.Column(db.String(50))
-    postcode = db.Column(db.String(10))
-    app_id = db.Column(db.UUID(), unique = True)
+#     id = db.Column(db.Integer, )
+#     patient = db.Column(primary_key=True)
+#     status = db.Column(db.String())
+#     time = db.Column(db.String(12))
+#     duration = db.Column(db.String(10))
+#     clinician = db.Column(db.String(50))
+#     department = db.Column(db.String(50))
+#     postcode = db.Column(db.String(10))
+#     app_id = db.Column(db.UUID(), unique = True)
 
-    def __init__(self, patient,name,DOB,postcode):
-        self.patient = patient
-        self.name = name
-        self.DOB = DOB
-        self.postcode = postcode
+#     def __init__(self, patient,name,DOB,postcode):
+#         self.patient = patient
+#         self.name = name
+#         self.DOB = DOB
+#         self.postcode = postcode
 
-    def __repr__(self):
-        return f"{self.patient}:{self.status}"
+#     def __repr__(self):
+#         return f"{self.patient}:{self.status}"
