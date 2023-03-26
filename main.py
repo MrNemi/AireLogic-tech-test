15548604e68a983aff32d92da0d4cef9a23cef23
Orion9551 <emmanuelombo95@gmail.com>
2023-03-27 00:39:23 +0100

    final edits to key app files

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask, request, redirect, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
 from models import db, PatientModel
 
 app = Flask(__name__)
 
 # Setup configs
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize database
 db.init_app(app)
@@ -24,7 +22,8 @@ def mainpage():
         name = request.form['name']
         dob = request.form['dob']
         postcode = request.form['postcode']
-        new_patient = PatientModel(nhs_number=nhs_number, name=name, dob=dob, postcode = postcode)
+        new_patient = PatientModel(nhs_number=nhs_number,\
+             name=name, dob=dob, postcode = postcode)
         
         try:
             db.session.add(new_patient)
@@ -34,9 +33,13 @@ def mainpage():
              return 'There was an issue adding new patient details'
         
     else:
-            patient = PatientModel.query.all()
-            return render_template('mainpage.html', patient=patient)
+        patient = PatientModel.query.all()
+        return render_template('mainpage.html', patient=patient)
     
+# Coding the Read/Retrieve views
+
+
+
 
 if __name__ == "__main__":
     app.run(host='localhost', port = 5000, debug =True)
--- models.py
+++ models.py
@@ -20,3 +20,25 @@ class PatientModel(db.Model):
 
     def __repr__(self):
         return '<Patient %r>' % self.id
+
+# Create Apppintment model
+class AppointmentModel(db.Model):
+    nhs_number = db.Column(db.Integer(), primary_key = True)
+    status = db.Column(db.String())
+    time = db.Column(db.String())
+    dur = db.Column(db.String())
+    clinician = db.Column(db.String(50))
+    dept = db.Column(db.String(50))
+    postcode = db.Column(db.String(10))
+
+    def __init__(self, nhs_number,status,time, dur,cln,dept,postcode):
+        self.nhs_number = nhs_number
+        self.status = status
+        self.time = time
+        self.dur= dur
+        self.cln = cln
+        self.dept = dept
+        self.postcode = postcode
+
+    def __repr__(self):
+        return '<Appointment %r>' % self.id
